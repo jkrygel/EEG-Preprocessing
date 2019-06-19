97d5f7e27d3b21f363d7387d12bb5bab961cdbb1
Tony Feng <yf19@queensu.ca>
2019-06-19 01:04:17 -0400

    Add example from demo, will be removed in future

--- Test/filter.py
+++ Test/filter.py
@@ -1,15 +1,18 @@
+import mne
 import pandas
 import numpy as np
 from mne.io import read_raw_bdf
 from mne.filter import notch_filter
 
 # raw data
-dat = read_raw_bdf("Subject_1-1.bdf")
-print(dat.info)
-##dat.plot()
+dat = read_raw_bdf("../Data/Subject_1-1.bdf")
+event = mne.events_from_annotations(dat)
+
+# print(dat.info)
+# dat.plot()
 
 # use EEG channel 1 to test
-eeg1 = dat.to_data_frame("EEG 1")
+# eeg1 = dat.to_data_frame("EEG 1")
 
 # apply notch filter, not sure about the parameters
-notch_filter(eeg1.values, 250, np.arange(60, 121, 60))
+# notch_filter(eeg1.values, 250, np.arange(60, 121, 60))
