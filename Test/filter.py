ccc01bf955a20b2ab9051f0a825f8de9e75a87c9
Tony Feng <yf19@queensu.ca>
2019-06-19 10:57:35 -0400

    drop channels

--- Test/filter.py
+++ Test/filter.py
@@ -10,6 +10,9 @@ event = mne.events_from_annotations(dat)
 info = dat.info
 sfreq = info.get("sfreq")
 
+# drop accelerometer channels
+dat.drop_channels(["Accel X", "Accel Y", "Accel Z"])
+
 # apply notch filter only on 60 Hz, default setting of OpenBCI
 # https://martinos.org/mne/stable/generated/mne.io.Raw.html?highlight=notch#mne.io.Raw.notch_filter
 dat.notch_filter(60)
@@ -18,13 +21,6 @@ dat.notch_filter(60)
 dat.filter(13, 25, n_jobs=1,        # use more jobs to speed up
             l_trans_bandwidth=1,    # make sure filter params are the same
             h_trans_bandwidth=1,    # in each band and skip "auto" option
-            fir_design='firwin')
+            fir_design="firwin")
 
-### use EEG channel 1 to test
-##eeg1 = dat.to_data_frame("EEG 1")
-##
-### apply notch filter on 60 Hz and 120 Hz
-####notched = notch_filter(eeg1.values, sfreq, np.arange(60, 121, 60)))
-##
-### apply notch filter only on 60 Hz, default setting of OpenBCI
-####notched = notch_filter(eeg1.values, sfreq, 60, filter_length=))
+dat.plot()
